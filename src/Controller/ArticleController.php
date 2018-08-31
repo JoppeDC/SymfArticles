94b57f170e124d2541645225ef58e5c6dc897058
JoppeDC <Joppedc@gmail.com>
2018-08-31 11:24:57 +0200

    Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,9 +8,12 @@
     namespace App\Controller;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Bundle\FrameworkBundle\FrameworkBundle\Configuration\Method;
 
     class ArticleController {
+        /**
+         * @Route("/")
+
+         */
         public function index(){
             return new Response('<html><body>Hello</body></html>');
         }
