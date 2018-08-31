d2d6fd6e15b6a84fecd8998e51bb552d5c68b354
JoppeDC <Joppedc@gmail.com>
2018-08-31 11:23:31 +0200

    Annotations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,4 +5,13 @@
  * Date: 31/08/2018
  * Time: 11:18
  */
-namespace App\Controller;
+    namespace App\Controller;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\FrameworkBundle\Configuration\Method;
+
+    class ArticleController {
+        public function index(){
+            return new Response('<html><body>Hello</body></html>');
+        }
+    }
