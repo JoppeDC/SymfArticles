a177a34eab9171652e5e6fed5be20ddcf0ce0ed0
JoppeDC <Joppedc@gmail.com>
2018-08-31 11:26:30 +0200

    Twig init

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,13 +8,13 @@
     namespace App\Controller;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-
-    class ArticleController {
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    class ArticleController extends Controller{
         /**
          * @Route("/")
 
          */
         public function index(){
-            return new Response('<html><body>Hello</body></html>');
+            return $this->render('articles/index.html.twig');
         }
     }
